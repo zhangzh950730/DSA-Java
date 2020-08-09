82a77b41f8c3e516dfbde6875d2caa150f1cd902
zhangzhihao <zhangzh950730@gmail.com>
2020-08-09 08:03:19 +0800

    串

---
+++ src/com/zzh/数据结构_邓俊辉/chapter11_串/String.java
@@ -0,0 +1,42 @@
+package com.zzh.数据结构_邓俊辉.chapter11_串;
+
+/**
+ * @author <a href="zhangzh950730@gmail.com" >ZhangZhiHao</a>
+ * @since 2020/8/8 11:29
+ */
+public class String {
+    private char[] data;
+    private int _length;
+
+    public int length() {
+        return _length;
+    }
+
+    public char charAt(int i) {
+        return 'a';
+    }
+
+    public String substr(int i, int k) {
+        return null;
+    }
+
+    public String prefix(int k) {
+        return null;
+    }
+
+    public String suffix(int k) {
+        return null;
+    }
+
+    public String concat(String s) {
+        return null;
+    }
+
+    public boolean equals(String s) {
+        return false;
+    }
+
+    public int indexOf(char p) {
+        return -1;
+    }
+}
