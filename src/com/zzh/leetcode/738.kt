dddd95b8fccee4500b20ec41573a805d2da7eab3
zhangzhihao <zhangzh950730@gmail.com>
2020-12-15 09:35:09 +0800

    738

---
+++ src/com/zzh/leetcode/738.kt
@@ -0,0 +1,32 @@
+package com.zzh.leetcode
+
+/**
+ * @author zhangzhihao
+ * @date 2020/12/15 8:36
+ */
+fun monotoneIncreasingDigits(N: Int): Int {
+    val strN = N.toString().toCharArray()
+    var i = 1
+    while (i < strN.size && strN[i - 1] <= strN[i]) {
+        i++
+    }
+    if (i < strN.size) {
+        while (i > 0 && strN[i - 1] > strN[i]) {
+            strN[i - 1] = strN[i - 1] - 1
+            i -= 1
+        }
+        for (j in i + 1 until strN.size) {
+            strN[j] = '9'
+        }
+    }
+    return String(strN).toInt()
+}
+
+fun main() {
+    var result = monotoneIncreasingDigits(10)
+    println("result = ${result}")
+    result = monotoneIncreasingDigits(1234)
+    println("result = ${result}")
+    result = monotoneIncreasingDigits(332)
+    println("result = ${result}")
+}
