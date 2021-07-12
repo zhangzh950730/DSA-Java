7b84e538336c9dea2d42902ac8766933362bdfda
ZhangZhiHao <zhangzh950730@gmail.com>
2021-07-12 23:32:03 +0800

    Merge sort

---
+++ src/com/zzh/algs4/chapter2/Merge.java
@@ -0,0 +1,50 @@
+package com.zzh.algs4.chapter2;
+
+import java.util.concurrent.ConcurrentMap;
+
+import static com.zzh.algs4.chapter2.ComparableUtils.less;
+import static com.zzh.algs4.chapter2.ComparableUtils.print;
+
+public class Merge {
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Character[] chars = new Character[]{'M', 'E', 'R', 'G', 'E', 'S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E'};
+        print(chars);
+        sort(chars);
+        print(chars);
+    }
+}
