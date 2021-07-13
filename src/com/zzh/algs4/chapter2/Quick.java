b516f6c682ecae05d6bb220f92f08eb0685c8f44
zhangzhihao_kuama <374752943@qq.com>
2021-07-13 20:03:42 +0800

    Quick

---
+++ src/com/zzh/algs4/chapter2/Quick.java
@@ -0,0 +1,53 @@
+package com.zzh.algs4.chapter2;
+
+import com.zzh.algs4.std.StdRandom;
+
+import static com.zzh.algs4.chapter2.ComparableUtils.exch;
+import static com.zzh.algs4.chapter2.ComparableUtils.less;
+
+/**
+ * @author ZhangZhiHao
+ * @date 2021/7/13 12:50
+ */
+public class Quick {
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        Comparable v = a[lo];
+        while (true) {
+            while (less(a[++i], v)) {
+                if (i == hi) {
+                    break;
+                }
+            }
+            while (less(v, a[--j])) {
+                if (j == lo) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
