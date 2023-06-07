8526fb55fef03c92b910d4025b3eb31a3a75d54d
zhangzh <zhangzh@gisway.com.cn>
2023-06-07 11:19:25 +0800

    876

---
+++ src/com/zzh/leetcode/_801_900/_876_middleNode.java
@@ -0,0 +1,49 @@
+package com.zzh.leetcode._801_900;
+
+import org.junit.Assert;
+
+/**
+ * @author <a href="mailto:374752943@qq.com">ZhiHao Zhang</a>
+ */
+public class _876_middleNode {
+
+    public static void main(String[] args) {
+        _876_middleNode middleNode = new _876_middleNode();
+        ListNode head, ans;
+
+        head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ans = middleNode.middleNode(head);
+        Assert.assertEquals(ans.val, 3);
+
+        head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+        ans = middleNode.middleNode(head);
+        Assert.assertEquals(ans.val, 4);
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
